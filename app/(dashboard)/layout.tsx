a12c9ff8c47140f15feb2040714b05e355eb2bbf
Olga Yudkin <cvtqx@protonmail.com>
2024-11-12 16:08:50 -0500

    added z-index to footer and sidenav and fixed to sidenav

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -3,11 +3,11 @@ import SideNav from './ui/sidenav';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className='grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]'>
-      <div className='hidden border-r md:block'>
+    <div className='relative min-h-screen w-full'>
+      <aside className='fixed top-0 left-0 w-[220px] md:w-[280px] h-full border-r bg-white z-0 hidden md:block'>
         <SideNav />
-      </div>
-      <div className='flex flex-col'>
+      </aside>
+      <div className='ml-0 md:ml-[280px] flex flex-col'>
         <MobileHeader />
         <main className='flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6'>
           {children}
--- app/global-ui/Footer.tsx
+++ app/global-ui/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='py-6 px-8 md:px-8 md:py-4 bg-merino'>
+    <footer className='py-6 px-8 md:px-8 md:py-4 bg-merino z-10 relative'>
       <div className='container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row '>
         <p className='text-gray text-center text-sm leading-loose text-muted-foreground md:text-left'>
           Built by{' '}
