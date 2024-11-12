b3fe1ea7323cc8cf3a876e2dec138bdb05d67c78
Olga Yudkin <cvtqx@protonmail.com>
2024-11-12 16:14:34 -0500

    removed scrollbar

--- app/(dashboard)/ui/sidenav.tsx
+++ app/(dashboard)/ui/sidenav.tsx
@@ -7,7 +7,7 @@ import FriendsList from './friendsList';
 
 export default function SideNav() {
   return (
-    <div className='flex h-full flex-col px-3 py-4 md:px-2 bg-merino'>
+    <div className='flex h-full flex-col px-3 py-4 md:px-2 bg-merino overflow-auto no-scrollbar'>
       <Link
         className='mb-2 flex h-20 items-center justify-center p-4 md:h-40'
         href='/home'>
